80770a74dce430cb68591549e65fb5cd2488f75e
Nandan331 <nanduumanyala@gmail.com>
2024-12-28 20:54:41 +0530

    update cors

--- Server/app.js
+++ Server/app.js
@@ -12,7 +12,7 @@ dotenv.config({path:"./process.env"});
 export const app = express();
 app.use(cors(
     {
-        origin:"http://localhost:3000", 
+        origin:"https://hotel-booking-wld1.onrender.com", 
         methods:[ 'GET', 'POST', 'PUT', 'UPDATE', 'DELETE' ],
         credentials:true
     } 
